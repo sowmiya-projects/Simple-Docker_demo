0dddaa40d4fa5617fd470721ee2d15983d14c952
sowmiya-projects <sowmithanam03@gmail.com>
2026-02-21 12:45:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("DevOps GHCR Demo App Running successfully - Sowmiya🚀");
+  res.send("DevOps GHCR Demo App Running successfully on port 3000 - Sowmiya🚀");
 });
 
 app.get("/add", (req, res) => {
