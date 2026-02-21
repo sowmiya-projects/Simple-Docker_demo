0859a9bce8a73b72d87b1521b588f4d2f85ee087
sowmiya <thanamsowmiya30@gmail.com>
2026-02-21 05:30:06 +0000

    my first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { add } = require("./calculator");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("DevOps GHCR Demo App Running 🚀");
+});
+
+app.get("/add", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  res.json({ result: add(a, b) });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
